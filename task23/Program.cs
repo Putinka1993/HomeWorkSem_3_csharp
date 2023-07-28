b3a1da4aba8634a4335de9df3a5053ad71c5c602
VladislavLipkinGit <lipkin1993vladislav@gmail.com>
2023-07-28 16:40:35 +0300

    task23 ready

---
+++ task23/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число ");
+int num = int.Parse(Console.ReadLine()!);
+
+int index = 1;
+
+while (index <= num)
+{
+    Console.WriteLine(Math.Pow(index, 3));
+    index++;
+}
