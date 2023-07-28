4716634446fc4e5124c57fe99cf0220a53753f08
VladislavLipkinGit <lipkin1993vladislav@gmail.com>
2023-07-28 16:36:13 +0300

    task19 ready

---
+++ task19/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Введите число ");
+string num = Console.ReadLine()!;
+
+if (num.Length == 5)
+{
+    if (num[0] + num[1] == num[4] + num[3])
+    {
+        Console.WriteLine("Число является палиндромом ");
+    }
+    else Console.WriteLine("Число не является палиндромом ");
+}
+else Console.WriteLine("Некорректное число, число должно быть пятизначным ");
